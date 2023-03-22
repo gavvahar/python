0ba94d49401ed6213a7f756070f981204a720b71
gavvahar <nihargavva@gmail.com>
2023-03-22 14:13:20 -0400

    url

--- full_name.py
+++ full_name.py
@@ -3,3 +3,7 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 message = f"{full_name.title()}!"
 print(message)
+
+nostarch_url = 'https://nostarch.com'
+simple_url = nostarch_url.removeprefix('https://')
+print(simple_url)

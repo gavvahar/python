075a505f088990fb93bf32eb78b576cb8f858baa
gavvahar <nihargavva@gmail.com>
2023-03-23 16:20:11 -0400

    done with chapter 3

--- Chapter3/motorcycles.py
+++ Chapter3/motorcycles.py
@@ -1,5 +1,10 @@
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
-motorcycles.append('ducati')
-print(motorcycles)
+motorcycles.insert(0, 'ducati')
+del motorcycles[2]
+del motorcycles[0]
+print(motorcycles[-1])
+
+popped_motorcycle = motorcycles.pop()
+print(popped_motorcycle)

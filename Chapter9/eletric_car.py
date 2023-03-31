c5df711e9ffa59184d1202f44f98829e70e0d637
gavvahar <nihargavva@gmail.com>
2023-03-30 20:35:13 -0400

    chapter 9

---
+++ Chapter9/eletric_car.py
@@ -0,0 +1,13 @@
+from car import Car
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class ."""
+        super().__init__(make, model, year)
+
+
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())

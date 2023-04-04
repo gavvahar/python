6873407a8d43746dc7f8972e2cf8832991b0b35b
gavvahar <nihargavva@gmail.com>
2023-04-04 19:15:56 -0400

    updating

--- Chapter11/test_survey.py
+++ Chapter11/test_survey.py
@@ -2,7 +2,7 @@ import pytest
 from survey import AnonymousSurvey
 
 
-# @pytest.fixture
+@pytest.fixture
 def language_survey():
     """A survey that will be available to all test functions."""
     question = "What language did you first learn to speak?"
